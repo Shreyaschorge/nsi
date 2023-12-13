343e72c9c202e5cc56f92e1b61699021d3e47109
Shreyaschorge <shreyas@neynar.com>
2023-12-13 23:01:05 +0530

    Add success callback

--- client.js
+++ client.js
@@ -1,29 +1,22 @@
 (function () {
   var authWindow;
 
-  // Function to handle the message from the authentication window
-  function handleMessage(event, authOrigin, callback) {
+  function handleMessage(event, authOrigin, successCallback) {
     if (event.origin === authOrigin && event.data.is_authenticated) {
-      // Process the authentication data
-      console.log("Authenticated:", event.data);
 
-      // Store authentication data in localStorage or handle it as needed
-      window.localStorage.setItem("user", JSON.stringify(event.data));
-
-      // Execute callback if provided
-      if (typeof callback === "function") {
-        callback();
+      if (typeof window[successCallback] === "function") {
+        window[successCallback](event.data); // Call the global callback function
       }
 
-      // Close the authentication window
       if (authWindow) {
         authWindow.close();
       }
+
+      window.removeEventListener("message", handleMessage);
     }
   }
 
-  // Function to handle the sign-in process
-  function handleSignIn(neynarLoginUrl, clientId, redirectUri, callback) {
+  function handleSignIn(neynarLoginUrl, clientId, redirectUri, successCallback) {
     var authUrl = new URL(neynarLoginUrl);
     authUrl.searchParams.append("client_id", clientId);
     if (redirectUri) {
@@ -35,39 +28,30 @@
     window.addEventListener(
       "message",
       function (event) {
-        handleMessage(event, authOrigin, callback);
+        handleMessage(event, authOrigin, successCallback);
       },
       false
     );
   }
 
-  // Function to create the sign-in button
   function createSignInButton(element) {
     var clientId = element.getAttribute("data-client_id");
     var neynarLoginUrl = element.getAttribute("data-neynar_login_url");
-    var redirectUri = element.getAttribute("data-redirect_uri"); // Optional
+    var redirectUri = element.getAttribute("data-redirect_uri");
+    var successCallback = element.getAttribute("data-success-callback");
 
     if (!clientId || !neynarLoginUrl) {
       console.error("Neynar Signin: Missing required data attributes");
       return;
     }
 
-    // Check if the button already exists
-    var existingButton = element.querySelector("button");
-    if (existingButton) {
-      return; // If button exists, do not create a new one
-    }
-
     var button = document.createElement("button");
     button.textContent = "Sign In with Neynar";
     button.onclick = function () {
-      handleSignIn(neynarLoginUrl, clientId, redirectUri, function () {
-        // Callback after successful authentication
-        // You can add additional logic here if needed
-      });
+      handleSignIn(neynarLoginUrl, clientId, redirectUri, successCallback);
     };
 
-    // Apply basic styles to the button
+    // Apply styles to the button
     button.style.padding = "10px 15px";
     button.style.border = "1px solid #ccc";
     button.style.borderRadius = "4px";
@@ -79,7 +63,6 @@
     element.appendChild(button);
   }
 
-  // Initialize the sign-in button when the DOM is fully loaded
   function init() {
     var signinElements = document.querySelectorAll(".neynar_signin");
     signinElements.forEach(createSignInButton);
