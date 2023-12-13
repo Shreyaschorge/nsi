00b832f6b892e993217cb65df88d9cf555fdf0c6
Shreyaschorge <shreyas@neynar.com>
2023-12-13 20:40:34 +0530

    store userdata in localstorage

--- client.js
+++ client.js
@@ -1,44 +1,54 @@
 (function () {
-  function initializeSignin() {
-    var signinElements = document.querySelectorAll(".neynar_signin");
-    // signinElements.forEach(function (signinElements) {
+  var clientId = "YOUR_CLIENT_ID";
+  var redirectUri = "YOUR_REDIRECT_URI";
+  var neynarLoginUrl = "YOUR_NEYNAR_LOGIN_URL";
+  var authWindow;
 
-    // });
-    var clientId = signinElements.getAttribute("data-client_id");
-    var loginUri = signinElements.getAttribute("data-login_uri");
-    var neynarLoginUrl = signinElements.getAttribute("data-neynar_login_url");
-
-    if (!clientId || !neynarLoginUrl) {
-      console.error("Neynar Signin: Missing required data attributes");
-      return;
-    }
-
-    signinElements.addEventListener("click", function () {
-      // Construct the URL for the authentication request
-      var authUrl = new URL(neynarLoginUrl);
-      authUrl.searchParams.append("client_id", clientId);
-      authUrl.searchParams.append("redirect_uri", loginUri);
-
-      // Open the authentication URL
-      window.open(authUrl.toString(), "_blank");
-    });
-
-    // Apply basic styles to the element
-    signinElements.style.cursor = "pointer";
-    signinElements.style.padding = "10px 15px";
-    signinElements.style.border = "1px solid #ccc";
-    signinElements.style.borderRadius = "4px";
-    signinElements.style.backgroundColor = "#fff";
-    signinElements.style.color = "#000";
-    signinElements.style.textAlign = "center";
-    signinElements.style.display = "inline-block";
-    signinElements.style.fontSize = "16px";
+  if (!clientId || !redirectUri || !neynarLoginUrl) {
+    console.error("Signin Button: Missing configuration");
+    return;
   }
 
-  // Wait for the DOM to be fully loaded
+  function handleMessage(event) {
+    var authOrigin = new URL(neynarLoginUrl).origin;
+    if (event.origin === authOrigin && event.data.is_authenticated) {
+      // Process the authentication data
+      console.log("Authenticated:", event.data);
+
+      window.localStorage.setItem("user", JSON.stringify(event.data));
+
+      // Remove the event listener
+      window.removeEventListener("message");
+
+      // Close the authentication window
+      if (authWindow) {
+        authWindow.close();
+      }
+    }
+  }
+
+  function handleSignIn() {
+    var authUrl = new URL(neynarLoginUrl);
+    authUrl.searchParams.append("client_id", clientId);
+    authUrl.searchParams.append("redirect_uri", redirectUri);
+    authWindow = window.open(authUrl.toString(), "_blank");
+    window.addEventListener("message", handleMessage, false);
+  }
+
+  function createSignInButton() {
+    var button = document.createElement("button");
+    button.textContent = "Sign In with Neynar";
+    button.onclick = handleSignIn;
+    // ... [style and append the button as in previous example] ...
+  }
+
+  function init() {
+    createSignInButton();
+  }
+
   if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", initializeSignin);
+    document.addEventListener("DOMContentLoaded", init);
   } else {
-    initializeSignin();
+    init();
   }
 })();
