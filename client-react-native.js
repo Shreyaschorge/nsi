f2e728d89c7872d104d2e665967460967d7eaa6e
Shreyaschorge <shreyas@neynar.com>
2023-12-29 00:50:08 +0530

    script for native app

--- client-react-native.js
+++ client-react-native.js
@@ -1,31 +1,6 @@
 (function () {
-  // Function to send a message to the React Native WebView
-  function sendMessageToReactNative(message) {
-    if (window.ReactNativeWebView) {
-      window.ReactNativeWebView.postMessage(message);
-    } else {
-      console.error("ReactNativeWebView is not defined.");
-    }
+  if (window.ReactNativeWebView) {
+    // Send a message to React Native to render the button
+    window.ReactNativeWebView.postMessage("renderButton");
   }
-
-  // Function to create a button and append it to the container
-  function createButton() {
-    var container = document.getElementById("neynar-container");
-    if (!container) {
-      console.error("The container element is not found.");
-      return;
-    }
-
-    var button = document.createElement("button");
-    button.innerHTML = "Sign In with Neynar";
-    button.onclick = function () {
-      // Send a message to the React Native app to open the WebView for login
-      sendMessageToReactNative("openLogin");
-    };
-
-    container.appendChild(button);
-  }
-
-  // Call the createButton function when the script loads
-  createButton();
 })();
